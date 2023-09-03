e6343f0b690fb0d518e0bedcd5cb0322d3177908
MBBrunito <tecnolastipas@gmail.com>
2023-09-03 13:43:42 -0300

    Recuperando ultimos datos

--- src/redux/noticiasActions/noticiasActions.js
+++ src/redux/noticiasActions/noticiasActions.js
@@ -1,38 +1,42 @@
 /* eslint-disable no-unused-vars */
-import { GET_ALL_NOTICIAS,GET_NOTICIA_DETAIL, FILTER_NOTICIAS } from "./noticiasActionTypes"
-import axios from "axios"
+import {
+   GET_ALL_NOTICIAS,
+   GET_NOTICIA_DETAIL,
+   FILTER_NOTICIAS,
+} from "./noticiasActionTypes";
+import axios from "axios";
 
 export function getAllNoticias() {
-    return async (dispatch) => {
-        try {
-            const { data } = await axios(`http://localhost:3001/notices`)
-            dispatch({type: GET_ALL_NOTICIAS, payload: data})
-        } catch (error) {
-            return alert(error.message)
-        }
-    }
+   return async (dispatch) => {
+      try {
+         const { data } = await axios(`http://localhost:3001/notices`);
+         dispatch({ type: GET_ALL_NOTICIAS, payload: data });
+      } catch (error) {
+         return alert(error.message);
+      }
+   };
 }
 
 export function getNoticiaDetail(id) {
-    return async (dispatch) => {
-        try {
-            const { data } = await axios(`http://localhost:3001/notices/${id}`)
-            dispatch({type: GET_NOTICIA_DETAIL, payload: data})
-        } catch (error) {
-            return alert(error.message)
-        }
-    }
+   return async (dispatch) => {
+      try {
+         const { data } = await axios(`http://localhost:3001/notices/${id}`);
+         dispatch({ type: GET_NOTICIA_DETAIL, payload: data });
+      } catch (error) {
+         return alert(error.message);
+      }
+   };
 }
 export function filteredNoticias(startDate, endDate) {
-    return async (dispatch) => {
-        try {
-            const { data } = await axios(`http://localhost:3001/notices/byDateRange?startDate=${startDate}&endDate=${endDate}`)
-            dispatch({type: FILTER_NOTICIAS, payload: data.data})
-            console.log(data.data)
-        } catch (error) {
-            return alert(error.message)
-        }
-    }
+   return async (dispatch) => {
+      try {
+         const { data } = await axios(
+            `http://localhost:3001/notices/byDateRange?startDate=${startDate}&endDate=${endDate}`
+         );
+         dispatch({ type: FILTER_NOTICIAS, payload: data.data });
+         console.log(data.data);
+      } catch (error) {
+         return alert(error.message);
+      }
+   };
 }
-
-
