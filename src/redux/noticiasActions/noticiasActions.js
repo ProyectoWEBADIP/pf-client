a845774e35bfdca57f2ea5def43d43d6383ccdc6
juanmanuel <losredondos551@gmail.com>
2023-09-03 22:59:06 -0300

    Filtro categoria y detail terminado

--- src/redux/noticiasActions/noticiasActions.js
+++ src/redux/noticiasActions/noticiasActions.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import { GET_ALL_NOTICIAS,GET_NOTICIA_DETAIL, FILTER_NOTICIAS , POST_NOTICIA,CLEAN_NOTICIA_DETAIL} from "./noticiasActionTypes"
+import { GET_ALL_NOTICIAS,GET_NOTICIA_DETAIL, FILTER_NOTICIAS , POST_NOTICIA,CLEAN_NOTICIA_DETAIL, GET_NOTICIAS_BY_CATEGORY} from "./noticiasActionTypes"
 import axios from "axios"
 
 export function getAllNoticias() {
@@ -30,7 +30,7 @@ export function getNoticiaDetail(id) {
     return async (dispatch) => {
         try {
             const { data } = await axios(`http://localhost:3001/notices/${id}`)
-            dispatch({type: GET_NOTICIA_DETAIL, payload: data})
+            dispatch({type: GET_NOTICIA_DETAIL, payload: data[0]})
         } catch (error) {
             return alert(error.message)
         }
@@ -52,6 +52,18 @@ export function filteredNoticias(startDate, endDate) {
             dispatch({type: FILTER_NOTICIAS, payload: data.data})
         } catch (error) {
             return alert(error.message)
+        }
+    }
+}
+
+export const getNoticiasByCategory = (id) => {
+    return async (dispatch) => {
+        try {
+            const { data } = await axios(`http://localhost:3001/notices/byCategory/${id}`)
+            console.log("dataaaa===Z", data);
+            dispatch({type: GET_NOTICIAS_BY_CATEGORY, payload: data})
+        } catch (error) {
+            console.log(error.message);
         }
     }
 }
