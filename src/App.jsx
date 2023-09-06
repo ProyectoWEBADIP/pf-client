16ab8f04729d48bb1bc644c229e3f2081ff2b0d9
Enzo Arjona <arjonaenzo1999@gmail.com>
2023-09-06 00:37:57 -0300

    dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-unused-vars */
-import { useSelector } from "react-redux";
-import CardsNoticias from "./components/CardsNoticias/CardsNoticias";
+
 import { Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
-import Login from "../src/components/Login/Login"
-import SingUp from "../src/components/Login/SingUp"
+import Login from "../src/components/Login/Login";
+import SingUp from "../src/components/Login/SingUp";
 import NoticiaDetail from "./components/detailNoticia/NoticiaDetail";
 import CrearNoticia from "./components/CraerNoticia/CrearNoticia";
-import PerfilUsuario from "./components/PerfilUsuario/Perfil"
-
+import PerfilUsuario from "./components/PerfilUsuario/Perfil";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { getDesignTokens } from "../helpers/theme";
+import React, { useState } from "react";
 
 function App() {
-  
+  const [themeMode, setThemeMode] = useState("light");
+
+  const toggleThemeMode = () => {
+    setThemeMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+  };
+
+  const theme = createTheme(getDesignTokens(themeMode));
+
   return (
     <div>
-      <Navbar/>
-      <Routes>
-      <Route path={"/"} element={<Home/>}/>
-      <Route path={"/login"} element={<Login/>}/>
-      <Route path={"/login/SignUp"} element={<SingUp/>}/>
-      <Route path={"/detalle/:id"} element={<NoticiaDetail/>}/>
-      <Route path={"/crearNoticia"} element={<CrearNoticia/>}/>
-      <Route path={"/:id/profile"} element={<PerfilUsuario/>}/>      
-      </Routes>
-      
-      
-
-
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar themeMode={themeMode} toggleThemeMode={toggleThemeMode} />
+        <Routes>
+          <Route path={"/"} element={<Home />} />
+          <Route path={"/login"} element={<Login />} />
+          <Route path={"/login/SignUp"} element={<SingUp />} />
+          <Route path={"/detalle/:id"} element={<NoticiaDetail />} />
+          <Route path={"/crearNoticia"} element={<CrearNoticia />} />
+          <Route path={"/:id/profile"} element={<PerfilUsuario />} />
+        </Routes>
+      </ThemeProvider>
     </div>
   );
 }
