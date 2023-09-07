537e0414b73fd5e56ff712d197704f3244cf6f92
Auliorp <aulioroveroparedes@gmail.com>
2023-09-06 23:00:54 -0300

    creacion de formulario para roles con sus validaciones de campos obligatorios

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Login from "../src/components/Login/Login";
 import SingUp from "../src/components/Login/SingUp";
 import NoticiaDetail from "./components/detailNoticia/NoticiaDetail";
 import CrearNoticia from "./components/CraerNoticia/CrearNoticia";
+import { CrearRol } from "./components/CrearRole/CrearRol";
 import PerfilUsuario from "./components/PerfilUsuario/Perfil";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -16,33 +17,34 @@ import { getDesignTokens } from "../helpers/theme";
 import React, { useState } from "react";
 
 function App() {
-  const storedThemeMode = localStorage.getItem("themeMode") || "light";
-  const [themeMode, setThemeMode] = useState(storedThemeMode);
+   const storedThemeMode = localStorage.getItem("themeMode") || "light";
+   const [themeMode, setThemeMode] = useState(storedThemeMode);
 
-  const toggleThemeMode = () => {
-    const prefThemeMode = themeMode === "light" ? "dark" : "light";
-    localStorage.setItem("themeMode", prefThemeMode);
-    setThemeMode(prefThemeMode);
-  };
+   const toggleThemeMode = () => {
+      const prefThemeMode = themeMode === "light" ? "dark" : "light";
+      localStorage.setItem("themeMode", prefThemeMode);
+      setThemeMode(prefThemeMode);
+   };
 
-  const theme = createTheme(getDesignTokens(themeMode));
+   const theme = createTheme(getDesignTokens(themeMode));
 
-  return (
-    <div>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Navbar themeMode={themeMode} toggleThemeMode={toggleThemeMode} />
-        <Routes>
-          <Route path={"/"} element={<Home />} />
-          <Route path={"/login"} element={<Login />} />
-          <Route path={"/login/SignUp"} element={<SingUp />} />
-          <Route path={"/detalle/:id"} element={<NoticiaDetail />} />
-          <Route path={"/crearNoticia"} element={<CrearNoticia />} />
-          <Route path={"/:id/profile"} element={<PerfilUsuario />} />
-        </Routes>
-      </ThemeProvider>
-    </div>
-  );
+   return (
+      <div>
+         <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Navbar themeMode={themeMode} toggleThemeMode={toggleThemeMode} />
+            <Routes>
+               <Route path={"/"} element={<Home />} />
+               <Route path={"/login"} element={<Login />} />
+               <Route path={"/login/SignUp"} element={<SingUp />} />
+               <Route path={"/detalle/:id"} element={<NoticiaDetail />} />
+               <Route path={"/crearNoticia"} element={<CrearNoticia />} />
+               <Route path={"/:id/profile"} element={<PerfilUsuario />} />
+               <Route path={"/crearRoles"} element={<CrearRol />} />
+            </Routes>
+         </ThemeProvider>
+      </div>
+   );
 }
 
 export default App;
