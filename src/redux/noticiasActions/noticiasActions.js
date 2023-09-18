3a71c291fcc9ae2a8e5be9099ac0141f74e7b97c
Gastón <gastibarossi@gmail.com>
2023-09-18 02:55:33 -0300

    asd

--- src/redux/noticiasActions/noticiasActions.js
+++ src/redux/noticiasActions/noticiasActions.js
@@ -8,8 +8,7 @@ import {
    
   NOTICIAS_PER_PAGE,
 } from "./noticiasActionTypes";
-import axios from '../../../axios-config';
-;
+import axios from 'axios'
 import {
   
   GET_NOTICIA_DETAIL,
@@ -31,7 +30,8 @@ import {
 export function getAllNoticias() {
    return async (dispatch) => {
       try {
-         const { data } = await axios(`/notices`);
+;
+const { data } = await axios(`/notices`);
          dispatch({ type: GET_ALL_NOTICIAS, payload: data });
       } catch (error) {
          console.log(error);
@@ -49,7 +49,7 @@ export function postNoticia(body) {
        
          console.log("entre a la action");
          const { data } = await axios.post(
-            `/notices`,
+            `https://backend-adipweb.onrender.com/notices`,
             body
             );
          dispatch({ type: POST_NOTICIA, payload: data });
@@ -62,7 +62,7 @@ export function postNoticia(body) {
 export function getNoticiaDetail(id) {
    return async (dispatch) => {
       try {
-         const { data } = await axios(`/notices/${id}`);
+         const { data } = await axios(`https://backend-adipweb.onrender.com/notices/${id}`);
          dispatch({ type: GET_NOTICIA_DETAIL, payload: data[0] });
       } catch (error) {
          return alert(error.message);
@@ -72,7 +72,7 @@ export function getNoticiaDetail(id) {
 export function getNoticiaDetailAdmin(id) {
    return async (dispatch) => {
       try {
-         const { data } = await axios(`/notices/${id}`);
+         const { data } = await axios(`https://backend-adipweb.onrender.com/notices/${id}`);
          return data;
       } catch (error) {
          return alert(error.message);
@@ -86,7 +86,7 @@ export function getNoticiasByTitle(title) {
             return dispatch({ type: CLEAN_FILTERS_NOTICIAS });
          }
          const { data } = await axios(
-            `/notices/byTitlePartial/${title}`
+            `https://backend-adipweb.onrender.com/notices/byTitlePartial/${title}`
          );
 
          return dispatch({ type: GET_NOTICIAS_BY_TITLE, payload: data.data });
