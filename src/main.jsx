0ba1a15c5a735a687349a1464eb2e3b83cba8c1a
Gastón <gastibarossi@gmail.com>
2023-09-18 02:50:11 -0300

    asd

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from "./redux/store.js";
-import axios from './axios-config';
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
