e47a7a4178b914e97dd4b5e2539ab9f07e4869ec
Gastón <gastibarossi@gmail.com>
2023-09-19 01:02:08 -0300

    cambiado browserRouter por HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import store from "./redux/store.js";
 import axios from "axios";
-// axios.defaults.baseURL= "http://localhost:3001"
-axios.defaults.baseURL= "https://backend-adipweb.onrender.com"
+axios.defaults.baseURL= "http://localhost:3001"
+// axios.defaults.baseURL= "https://backend-adipweb.onrender.com"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <React.StrictMode>
         <App />
       </React.StrictMode>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
