20555441fae14c7cb3943d8c0739976fa9e1e408
Dawyid22 <dawyid43@gmail.com>
2023-09-20 03:42:22 -0300

    "commit de baneo"

--- src/components/PerfilUsuario/Perfil.jsx
+++ src/components/PerfilUsuario/Perfil.jsx
@@ -248,6 +248,7 @@ export default function Perfil() {
                       </Select>
                     </FormControl>
 
+
                     <Toolbar>
                       <TextField
                         margin="normal"
