76d6d1a82291ef884d08765655fc5fa307270232
Auliorp <aulioroveroparedes@gmail.com>
2023-09-21 06:01:20 -0300

    df

--- axios-config.js
+++ axios-config.js
@@ -1,11 +1,10 @@
 // axios-config.js
 
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'https://backend-adipweb.onrender.com', // Reemplaza esto con tu URL base
-  // baseURL: 'http://localhost:3001', 
-
+   /* baseURL: 'https://backend-adipweb.onrender.com' */ // Reemplaza esto con tu URL base
+   baseURL: "http://localhost:3001",
 });
 
 export default instance;
