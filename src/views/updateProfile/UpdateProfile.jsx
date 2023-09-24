89f61a9744d0e159c8e4db5e6bf5318d98c40547
MBBrunito <tecnolastipas@gmail.com>
2023-09-24 12:38:58 -0300

    Update Profile Responsive

--- src/views/updateProfile/UpdateProfile.jsx
+++ src/views/updateProfile/UpdateProfile.jsx
@@ -1,274 +1,285 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
-import './updateProfile.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { getUserById } from '../../redux/login-registerActions/loginActions';
+import React, { useState } from "react";
+import "./updateProfile.css";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserById } from "../../redux/login-registerActions/loginActions";
 import {
-  Alert,
-  Box,
-  FormControlLabel,
-  InputAdornment,
-  Radio,
-  RadioGroup,
-  Select,
-  TextField,
-  ToggleButton,
-  ToggleButtonGroup,
-} from '@mui/material';
-import { MenuItem } from 'react-pro-sidebar';
-import BadgeIcon from '@mui/icons-material/Badge';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import FileUploadIcon from '@mui/icons-material/FileUpload';
-import { AccountCircle, Cake, Close, Phone, Save } from '@mui/icons-material';
+   Alert,
+   Box,
+   FormControlLabel,
+   InputAdornment,
+   Radio,
+   RadioGroup,
+   Select,
+   TextField,
+   ToggleButton,
+   ToggleButtonGroup,
+} from "@mui/material";
+import { MenuItem } from "react-pro-sidebar";
+import BadgeIcon from "@mui/icons-material/Badge";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import FileUploadIcon from "@mui/icons-material/FileUpload";
+import { AccountCircle, Cake, Close, Phone, Save } from "@mui/icons-material";
 import {
-  submitImgToCloudinary,
-  unshowProfileEdit,
-  updateUserProfile,
-} from '../../redux/profileActions/profileActions';
-import AlertSuccess from '../../assets/AlertSuccess/AlertSuccess';
+   submitImgToCloudinary,
+   unshowProfileEdit,
+   updateUserProfile,
+} from "../../redux/profileActions/profileActions";
+import AlertSuccess from "../../assets/AlertSuccess/AlertSuccess";
 const UpdateProfile = ({ perfilUsuario }) => {
-  const dispatch = useDispatch();
-  const [updatedFields, setUpdatedFields] = React.useState({
-    firstName: perfilUsuario.profile.firstName,
-    lastName: perfilUsuario.profile.lastName,
-    birthDate: perfilUsuario.profile.birthDate,
-    gender: perfilUsuario.profile.gender,
-    image: perfilUsuario.profile.image,
-    dni: perfilUsuario.profile.dni,
-    phone: perfilUsuario.profile.phone,
-  });
-  const [imageURL, setImageURL] = useState(''); //url
-  const [file, setFile] = useState(null);
-  const [success, setSuccess] = useState(false);
-  const [showAlert, setShowAlert] = useState(false);
-  const [imgUpdated, setImgUpdated] = useState(false);
-  const [alert, setAlert] = useState('');
-  const [changes, setChanges] = useState(false)
+   const dispatch = useDispatch();
+   const [updatedFields, setUpdatedFields] = React.useState({
+      firstName: perfilUsuario.profile.firstName,
+      lastName: perfilUsuario.profile.lastName,
+      birthDate: perfilUsuario.profile.birthDate,
+      gender: perfilUsuario.profile.gender,
+      image: perfilUsuario.profile.image,
+      dni: perfilUsuario.profile.dni,
+      phone: perfilUsuario.profile.phone,
+   });
+   const [imageURL, setImageURL] = useState(""); //url
+   const [file, setFile] = useState(null);
+   const [success, setSuccess] = useState(false);
+   const [showAlert, setShowAlert] = useState(false);
+   const [imgUpdated, setImgUpdated] = useState(false);
+   const [alert, setAlert] = useState("");
+   const [changes, setChanges] = useState(false);
 
-  function handleChange(e) {
-    setChanges(true)
+   function handleChange(e) {
+      setChanges(true);
 
-    if (e.target.name === 'image') {
-      setImgUpdated(true);
-      const file = e.target.files[0];
-      setFile(e.target.files[0]);
-      const path = URL.createObjectURL(file);
-      setImageURL(path);
-    }
-    if (e.target.name === 'dni' && updatedFields.dni.length < 8) {
-      setUpdatedFields({
-        ...updatedFields,
-        [e.target.name]: e.target.value,
-      });
-    }
-    setUpdatedFields({
-      ...updatedFields,
-      [e.target.name]: e.target.value,
-    });
-  }
-  const [loading, setLoading] = useState(false);
-  async function handleUpload(e) {
-    if(changes){
-      if (imgUpdated) {
-        setLoading(true)
-        const cloudinaryResponse = await dispatch(submitImgToCloudinary(file));
-        updatedFields.image = cloudinaryResponse.secure_url;
-        setAlert(cloudinaryResponse.message);
-        setShowAlert(true);
-        setTimeout(() => {
-          setShowAlert(false);
-        }, 5000);
+      if (e.target.name === "image") {
+         setImgUpdated(true);
+         const file = e.target.files[0];
+         setFile(e.target.files[0]);
+         const path = URL.createObjectURL(file);
+         setImageURL(path);
       }
-      const id = perfilUsuario.profile.id;
-      setLoading(true)
-      const response = await dispatch(updateUserProfile(id, updatedFields));
-      setLoading(false)
-      setImgUpdated(false);
-      setSuccess(`${response} La página se refrescará.`);
-      setTimeout(() => {
-        location.reload()
-        setSuccess(false);
-        
-      }, 3000);
-      return;
-    } else{
-      setSuccess(`No hay cambios que aplicar.`);
-      setTimeout(() => {
-        setSuccess(false);
-        
-      }, 3000);
-    }
-  }
-  return (
-    <div className="overlayUpdateModal">
-      <div className={localStorage.themeMode==='light'?"updateModal light":"updateModal dark"}>
-        { loading ? (
-          <div className="loading-update-profile">
-            <div className="lds-roller">
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
+      if (e.target.name === "dni" && updatedFields.dni.length < 8) {
+         setUpdatedFields({
+            ...updatedFields,
+            [e.target.name]: e.target.value,
+         });
+      }
+      setUpdatedFields({
+         ...updatedFields,
+         [e.target.name]: e.target.value,
+      });
+   }
+   const [loading, setLoading] = useState(false);
+   async function handleUpload(e) {
+      if (changes) {
+         if (imgUpdated) {
+            setLoading(true);
+            const cloudinaryResponse = await dispatch(
+               submitImgToCloudinary(file)
+            );
+            updatedFields.image = cloudinaryResponse.secure_url;
+            setAlert(cloudinaryResponse.message);
+            setShowAlert(true);
+            setTimeout(() => {
+               setShowAlert(false);
+            }, 5000);
+         }
+         const id = perfilUsuario.profile.id;
+         setLoading(true);
+         const response = await dispatch(updateUserProfile(id, updatedFields));
+         setLoading(false);
+         setImgUpdated(false);
+         setSuccess(`${response} La página se refrescará.`);
+         setTimeout(() => {
+            location.reload();
+            setSuccess(false);
+         }, 3000);
+         return;
+      } else {
+         setSuccess(`No hay cambios que aplicar.`);
+         setTimeout(() => {
+            setSuccess(false);
+         }, 3000);
+      }
+   }
+   const fecha = new Date(updatedFields.birthDate);
+   const fechaFormateada = fecha.toISOString().split("T")[0];
+   return (
+      <div className="overlayUpdateModal">
+         <div
+            className={
+               localStorage.themeMode === "light"
+                  ? "updateModal light"
+                  : "updateModal dark"
+            }
+         >
+            {loading ? (
+               <div className="loading-update-profile">
+                  <div className="lds-roller">
+                     <div></div>
+                     <div></div>
+                     <div></div>
+                     <div></div>
+                     <div></div>
+                     <div></div>
+                     <div></div>
+                     <div></div>
+                  </div>
+               </div>
+            ) : null}
+            {showAlert && (
+               <div className="imageAlertModal">
+                  <Alert severity="success">{alert}</Alert>
+               </div>
+            )}
+            {success && (
+               <div className="successUpdate">
+                  <AlertSuccess success={success} />
+               </div>
+            )}
+            <div
+               className="closeButtonContainer"
+               onClick={() => dispatch(unshowProfileEdit())}
+            >
+               <Close fontSize="medium" />
             </div>
-          </div>
-        ) : null}
-        {showAlert && (
-          <div className="imageAlertModal">
-            <Alert severity="success">{alert}</Alert>
-          </div>
-        )}
-        {success && (
-          <div className="successUpdate">
-            <AlertSuccess success={success} />
-          </div>
-        )}
-        <div
-          className="closeButtonContainer"
-          onClick={() => dispatch(unshowProfileEdit())}
-        >
-          <Close fontSize="medium" />
-        </div>
-        <div className="titleModal">
-          <span>Editar perfil</span>
-        </div>
-        <div className="namesImgCont">
-          <div className="namesModal">
-            <TextField
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <AccountCircle />
-                  </InputAdornment>
-                ),
-              }}
-              name="firstName"
-              placeholder={perfilUsuario.profile.firstName}
-              id="outlined-basic"
-              onChange={handleChange}
-              label="Nombre"
-              variant="outlined"
-            />
-            <TextField
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <AccountCircle />
-                  </InputAdornment>
-                ),
-              }}
-              name="lastName"
-              placeholder={perfilUsuario.profile.lastName}
-              onChange={handleChange}
-              id="outlined-basic"
-              label="Apellido"
-              variant="outlined"
-            />
-          </div>
-          <div className="imgModal">
-            <div>
-              <img
-                src={imageURL ? imageURL : perfilUsuario.profile.image}
-                alt=""
-              />
+            <div className="titleModal">
+               <span>Editar perfil</span>
             </div>
-            <div className="fileContModal">
-              <input
-                onChange={handleChange}
-                name="image"
-                id="uploadImgInput"
-                type="file"
-              />
-              <label htmlFor="uploadImgInput">
-                {' '}
-                <FileUploadIcon fontSize="large" />
-              </label>
+            <div className="namesImgCont">
+               <div className="namesModal">
+                  <TextField
+                     InputProps={{
+                        startAdornment: (
+                           <InputAdornment position="start">
+                              <AccountCircle />
+                           </InputAdornment>
+                        ),
+                     }}
+                     name="firstName"
+                     placeholder={perfilUsuario.profile.firstName}
+                     id="outlined-basic"
+                     onChange={handleChange}
+                     label="Nombre"
+                     variant="outlined"
+                  />
+                  <TextField
+                     InputProps={{
+                        startAdornment: (
+                           <InputAdornment position="start">
+                              <AccountCircle />
+                           </InputAdornment>
+                        ),
+                     }}
+                     name="lastName"
+                     placeholder={perfilUsuario.profile.lastName}
+                     onChange={handleChange}
+                     id="outlined-basic"
+                     label="Apellido"
+                     variant="outlined"
+                  />
+               </div>
+               <div className="imgModal">
+                  <div>
+                     <img
+                        src={imageURL ? imageURL : perfilUsuario.profile.image}
+                        alt=""
+                     />
+                  </div>
+                  <div className="fileContModal">
+                     <input
+                        onChange={handleChange}
+                        name="image"
+                        id="uploadImgInput"
+                        type="file"
+                     />
+                     <label htmlFor="uploadImgInput">
+                        {" "}
+                        <FileUploadIcon fontSize="large" />
+                     </label>
+                  </div>
+               </div>
             </div>
-          </div>
-        </div>
-        <div className="birth-gender-imgModal">
-          <TextField
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <Cake />
-                </InputAdornment>
-              ),
-            }}
-            name="birthDate"
-            type="date"
-            focused
-            label="Fecha de Nacimiento"
-            onChange={handleChange}
-          />
-          <RadioGroup
-            onChange={handleChange}
-            row
-            aria-labelledby="demo-radio-buttons-group-label"
-            defaultValue={perfilUsuario.profile.gender}
-            name="gender"
-          >
-            <FormControlLabel
-              value="Femenino"
-              control={<Radio />}
-              label="Femenino"
-            />
-            <FormControlLabel
-              value="Masculino"
-              control={<Radio />}
-              label="Masculino"
-            />
-          </RadioGroup>
-          <TextField
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <Phone />
-                </InputAdornment>
-              ),
-            }}
-            placeholder={perfilUsuario.profile.phone}
-            id="outlined-basic"
-            onChange={handleChange}
-            label="Teléfono"
-            name="phone"
-            variant="outlined"
-          />
-        </div>
-        <div className="dniModal">
-          <TextField
-            InputProps={{
-              maxLength: 10,
+            <div className="birth-gender-imgModal">
+               <TextField
+                  InputProps={{
+                     startAdornment: (
+                        <InputAdornment position="start">
+                           <Cake />
+                        </InputAdornment>
+                     ),
+                  }}
+                  name="birthDate"
+                  type="date"
+                  focused
+                  value={fechaFormateada}
+                  label="Fecha de Nacimiento"
+                  onChange={handleChange}
+               />
+               <RadioGroup
+                  onChange={handleChange}
+                  row
+                  aria-labelledby="demo-radio-buttons-group-label"
+                  defaultValue={perfilUsuario.profile.gender}
+                  name="gender"
+               >
+                  <FormControlLabel
+                     value="Femenino"
+                     control={<Radio />}
+                     label="Femenino"
+                  />
+                  <FormControlLabel
+                     value="Masculino"
+                     control={<Radio />}
+                     label="Masculino"
+                  />
+               </RadioGroup>
+               <TextField
+                  InputProps={{
+                     startAdornment: (
+                        <InputAdornment position="start">
+                           <Phone />
+                        </InputAdornment>
+                     ),
+                  }}
+                  placeholder={perfilUsuario.profile.phone}
+                  id="outlined-basic"
+                  onChange={handleChange}
+                  label="Teléfono"
+                  name="phone"
+                  variant="outlined"
+               />
+            </div>
+            <div className="dniModal">
+               <TextField
+                  InputProps={{
+                     maxLength: 10,
 
-              startAdornment: (
-                <InputAdornment position="start">
-                  <BadgeIcon />
-                </InputAdornment>
-              ),
-            }}
-            id="outlined-basic"
-            onChange={handleChange}
-            label="DNI"
-            name="dni"
-            variant="outlined"
-            maxLength="8"
-            placeholder={perfilUsuario.profile.dni}
-          />
-        </div>
-       {loading? null:<div onClick={handleUpload} className="saveButton">
-          <span>
-            <Save fontSize="large" /> Guardar cambios
-          </span>
-        </div>}
+                     startAdornment: (
+                        <InputAdornment position="start">
+                           <BadgeIcon />
+                        </InputAdornment>
+                     ),
+                  }}
+                  id="outlined-basic"
+                  onChange={handleChange}
+                  label="DNI"
+                  name="dni"
+                  variant="outlined"
+                  maxLength="8"
+                  placeholder={perfilUsuario.profile.dni}
+               />
+            </div>
+            {loading ? null : (
+               <div onClick={handleUpload} className="saveButton">
+                  <span>
+                     <Save fontSize="large" /> Guardar cambios
+                  </span>
+               </div>
+            )}
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default UpdateProfile;
